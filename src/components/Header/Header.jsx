2c5d2441427b0be74fad9ee70972314f80ce7361
PesukarhuTG <ganzhinovat@yandex.ru>
2024-07-31 23:41:33 +0300

    feat: add burger menu element

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,9 +43,25 @@ const Header = () => {
           </ul>
         </nav>
 
-        <a className={style.header__cartLink} href="cart.html">
-          6
-        </a>
+        <div className={style.header__iconWrapper}>
+          <a className={style.header__cartLink} href="cart.html">
+            6
+          </a>
+
+          <button className={style.header__burger}>
+            <svg
+              width="28"
+              height="29"
+              viewBox="0 0 28 29"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect x="4" y="9.5" width="20" height="1" fill="#D9D9D9" />
+              <rect x="4" y="14.5" width="20" height="1" fill="#D9D9D9" />
+              <rect x="4" y="19.5" width="20" height="1" fill="#D9D9D9" />
+            </svg>
+          </button>
+        </div>
       </div>
     </header>
   );
