68f73ee6a72b68620a4092dafe4750f97af4b718
PesukarhuTG <ganzhinovat@yandex.ru>
2024-08-04 23:37:27 +0300

    feat: add condition for button in Promo component

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,17 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import style from './Promo.module.css';
 
-const Promo = () => (
-  <section className={style.promo}>
-    <div className={style.container}>
-      <div className={style.promo__container}>
-        <h1 className={style.promo__title}>Попробуй новый вкус Арабики</h1>
-        <Link className={style.promo__link} to="/products?category=coffee">
-          Перейти к&nbsp;кофе
-        </Link>
+const Promo = () => {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get('category');
+
+  return (
+    <section className={style.promo}>
+      <div className={style.container}>
+        <div className={style.promo__container}>
+          <h1 className={style.promo__title}>Попробуй новый вкус Арабики</h1>
+          {category !== 'coffee' ? (
+            <Link className={style.promo__link} to="/products?category=coffee">
+              Перейти к&nbsp;кофе
+            </Link>
+          ) : null}
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Promo;
